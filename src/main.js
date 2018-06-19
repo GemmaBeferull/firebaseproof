ecffd2cc09f278223148878ca7b453a8460bc8ae
Gemma Beferull Calabuig <gemmabecala@gmail.com>
2018-06-19 10:42:19 +0200

    bucle infinito

--- src/main.js
+++ src/main.js
@@ -8,28 +8,21 @@ var config = {
   messagingSenderId: "785154341794"
 };
 firebase.initializeApp(config);
-
-/* */
-// var pre = document.getElementById('objeto');
-// var dbRefObject = firebase.database().ref().child('rita'); 
-
-/*'value' detecta cambios en contenido 
-   on()  llama a la funcion cuando pasa el evento (en este caso 'value')
-   once() llama a la funcion cuando pasa el evento pero solo una vez
-   off() deja de escuchar el evento
- */
-// dbRefObject.on('value', snap => {
-//     console.log(snap.val())
-//     pre.innerHTML = JSON.stringify(snap.val());
-// });
-console.log('hola');
 }())
 
 var mailInput = document.getElementById('mailInput');
 var passwordInput = document.getElementById('passwordInput');
 var loginButton = document.getElementById('loginButton');
 var registerButton = document.getElementById('registerButton');
-
+var formArea = document.getElementById('formulario');
+var logginArea = document.querySelector('.loginArea');
+var nombre = document.querySelector('.nombre');
+var apellido = document.querySelector('.apellido');
+var guardarButton = document.getElementById('saveButton');
+var show = document.querySelector('.show');
+var showName = document.querySelector('.showName');
+var showSurname = document.querySelector('.showlastname');
+var showEmail = document.querySelector('.showEmail');
 
 
 loginButton.addEventListener('click', e => {
@@ -40,7 +33,7 @@ loginButton.addEventListener('click', e => {
     
     const promise = auth.signInWithEmailAndPassword(mail, password);
     promise.catch (e => console.log (e.message))
-    hola()
+    
 });
 
 registerButton.addEventListener('click', e => {
@@ -50,7 +43,7 @@ registerButton.addEventListener('click', e => {
     
     const promise = auth.createUserWithEmailAndPassword(mail, password);
     promise.catch (e => console.log (e.message))
-    hola();
+    
 });
 
 var logoutButton = document.getElementById('logoutButton');
@@ -59,22 +52,26 @@ var logoutButton = document.getElementById('logoutButton');
 logoutButton.addEventListener('click', e => {
     const auth = firebase.auth();
     const promise = auth.signOut();
+    logginArea.classList.remove('hidden');
+    logoutButton.classList.add('hidden');
+    formArea.classList.add('hidden');
+    show.classList.add('hidden');
 
 });
 
- function hola(){
+ 
 firebase.auth().onAuthStateChanged(firebaseUser => {
 
     if (firebaseUser) {
         
         console.log(firebaseUser);
-        //logoutButton.classList.remove('hidden');
+        logginArea.classList.add('hidden');
+        logoutButton.classList.remove('hidden');
+        formArea.classList.remove('hidden');
       
         const userReference = firebase.database().ref(`users/${firebaseUser.uid}`);
         userReference.once('value', snapshot => {
-            
             if (!snapshot.val()) {
-                
                 // User does not exist, create user entry
                 console.log(mailInput.value)
                 userReference.set({
@@ -82,13 +79,29 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
                  });
             }
         });
-        console.log(firebaseUser.uid);
-        // window.location = 'form.html';
-        //setLoggedUserState();
+        guardarButton.addEventListener('click', ()=>{
+            event.preventDefault()
+                userReference.on('value', snapshot => {
+                        userReference.set({
+                            email: mailInput.value,
+                            nombre: nombre.value,
+                            apellido: apellido.value
+                         });
+                });
+                
+                formArea.classList.add('hidden');
+                show.classList.remove('hidden');
+                var dbRefObject = firebase.database().ref().child(`users/${firebaseUser.uid}`)
+                dbRefObject.on('value', snap => {
+                        console.log(snap.val());
+                    showName.innerHTML = snap.val().nombre;
+                    showSurname.innerHTML = snap.val().apellido;
+                    showEmail.innerHTML = snap.val().email
+                    });
+         }) 
         }else {
             console.log('no logueado');
-            //logoutButton.classList.add('hidden');
-            //setLoggedOutUserState();
+           
         }
  });
-}
+
